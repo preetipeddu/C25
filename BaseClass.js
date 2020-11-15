6dc4967757829aad1c33f96b627c22a4ebedf2f3
preetipeddu <72349342+preetipeddu@users.noreply.github.com>
2020-11-15 13:35:55 -0500

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,29 @@
+class BaseClass{
+    constructor(x,y,width,height,angle){
+        var options = {
+            'density':1.5,
+            'friction': 1.0,
+            'restitution':0.5
+          };
+
+          this.body = Bodies.rectangle(x,y,width,height,options);
+
+          this.image = loadImage('sprites/base.png');
+          this.w = width;
+          this.h = height;
+          World.add(world, this.body);
+    }
+
+
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0,0, this.w, this.h);
+        pop();
+
+    }
+}
